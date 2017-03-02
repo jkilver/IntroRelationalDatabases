3e687417b5dda8621d61c7d7867697947fceaf69
Jacob Kilver <robobookworm@gmail.com>
2017-03-01 20:28:29 -0500

    Implemented database interface for forum

--- Forum/forumdb.py
+++ Forum/forumdb.py
@@ -3,9 +3,10 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
-DB = []
+#DB = psycopg2.connect("dbname=forum")
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +17,15 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("select * from posts")
+    rows = cursor.fetchall()
+    
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in rows]
     posts.sort(key=lambda row: row['time'], reverse=True)
+    
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +35,9 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    DB = psycopg2.connect("dbname=forum")
     t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    cursor = DB.cursor()
+    cursor.execute("insert into posts values(%s, %s)", (content, t))
+    DB.commit()
+    DB.close()
