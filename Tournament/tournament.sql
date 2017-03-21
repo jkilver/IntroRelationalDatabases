e954286b99825aa3494ebd057ddf1adf16050986
Jacob Kilver <robobookworm@gmail.com>
2017-03-20 22:15:07 -0400

    Finalized sql database creation

--- Tournament/tournament.sql
+++ Tournament/tournament.sql
@@ -7,26 +7,30 @@
 -- these lines here.
 
 -- Remove old tournament database if it exists
-
+DROP DATABASE IF EXISTS tournament;
 
 -- Create database and connect to it
 CREATE DATABASE tournament;
-/c tournament;
+\c tournament;
 
 -- Create table for player information
-CREATE TABLE players
-    id as serial integer
-    name as string;
+CREATE TABLE players (
+    player_id serial PRIMARY KEY,
+    name text);
  
 -- Create table for match information 
-CREATE TABLE matches
-    match_id as serial integer
-    player1 as integer from players.id
-    player2 as integer from players.id
-    winner as integer from players.id
-    loser as integer from players.id
+CREATE TABLE matches (
+    match_id serial PRIMARY KEY,
+    player1 integer REFERENCES players(player_id),
+    player2 integer REFERENCES players(player_id),
+    winner  integer REFERENCES players(player_id),
+    loser   integer REFERENCES players(player_id) );
    
 -- Create view to easily access win/loss record of players   
-CREATE VIEW 
+CREATE VIEW playerStandings AS 
+    SELECT player_id, 
+    (SELECT count(*) FROM matches WHERE winner = player_id) as wins,
+    (SELECT count(*) FROM matches WHERE loser = player_id) as losses
+    FROM players;
 
 
